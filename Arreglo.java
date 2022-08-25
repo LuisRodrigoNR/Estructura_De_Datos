bfcbe6c0b736e22eb7fc8b154257e4819a29cf2e
LuisRodrigoNR <91902774+LuisRodrigoNR@users.noreply.github.com>
2022-08-25 00:56:42 -0500

    Update Arreglo.java

--- Arreglo.java
+++ Arreglo.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Arreglo {
 	
-private int tamanio;
+private int tamanio;  //atributos
 private int indice;
 private String valor;
 
@@ -10,14 +10,14 @@ Arreglo (int tamanio) { //constructor
 	this.tamanio=tamanio;
 }
 
-public int getLongitud() {
+public int getLongitud() {   //obtiene el tamaño del arreglo
 	return this.tamanio;
 }
-public void setElemento(int indice, String valor) {
+public void setElemento(int indice, String valor) {    //Establece un elemnto
 	this.indice=indice;
 	this.valor=valor;
 }
-public int getElemento(int indice){
+public int getElemento(int indice){      
     if(indice>=this.tamanio || indice<0){
         System.out.println("Indice fuera del limite");
     }
@@ -25,12 +25,12 @@ public int getElemento(int indice){
 
 	        
 }
-public void limpiar(String valor){
+public void limpiar(String valor){  //limpia la pantalla con el valor proporcionado
     
 }
 
    @Override
-public String toString() {
+public String toString() {   //muestra el estado del objeto
 	return "Arreglo [tamanio=" + tamanio + ", indice=" + indice + ", valor=" + valor + "]";
 }
 
